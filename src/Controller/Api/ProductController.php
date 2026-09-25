d257c9ec627bad0cc5ed883b1ffd682778fdcae3
kkamara <kamaracomputers@gmail.com>
2026-09-25 17:17:52 +0100

    Also, serialize response data explicitly in controller

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -7,14 +7,20 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class ProductController extends AbstractController
 {
     #[Route('/api/products', methods: ["GET"])]
-    public function index(EntityManagerInterface $em): JsonResponse
+    public function index(
+        EntityManagerInterface $em,
+        SerializerInterface $serializer,
+    ): JsonResponse
     {
         $products = $em->getRepository(Product::class)->findAll();
 
-        return $this->json($products);
+        $json_content = $serializer->serialize($products, 'json');
+
+        return JsonResponse::fromJsonString($json_content);
     }
 }
