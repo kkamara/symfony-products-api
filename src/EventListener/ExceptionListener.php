9560b304c6d7e84727fbf9fbcfa992b19a068dc7
kkamara <kamaracomputers@gmail.com>
2026-09-25 17:58:47 +0100

    Add exception listener

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -3,6 +3,7 @@
 namespace App\EventListener;
 
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
 final class ExceptionListener
@@ -10,6 +11,20 @@ final class ExceptionListener
     #[AsEventListener]
     public function onExceptionEvent(ExceptionEvent $event): void
     {
-        // ...
+        $request = $event->getRequest();
+
+        $path = $request->getPathInfo();
+
+        if (false === strpos($path, '/api')) {
+            return;
+        }
+
+        $exception = $event->getThrowable();
+
+        $response = new JsonResponse([
+            "error" => $exception->getMessage(),
+        ]);
+
+        $event->setResponse($response);
     }
 }
